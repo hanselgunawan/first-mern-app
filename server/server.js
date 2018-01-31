163b94bf0eaf9153e7ba86ebacb7accdaf6dc4fb
hansel.tritama <hanselgunawan94@gmail.com>
2018-01-31 13:05:24 -0800

    Added mLab path and changed port

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 // mongoose.connect('mongodb://<hanselgunawan>:<shiro9111>@ds263847.mlab.com:63847/mern-practice');
 
-mongoose.connect('mongodb://localhost/new_york_times_db');
+mongoose.connect('mongodb://heroku_z2kf60vq:nfg5vl35guh7gnu3pmrvamn4oi@ds121248.mlab.com:21248/heroku_z2kf60vq');
 
 app.use('/', router);
 
